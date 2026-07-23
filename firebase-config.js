fdad5fb2faed5d7720c0337f2493542830b62bee
soccerykohi-eng <soccerykohi@gmail.com>
2026-07-23 12:02:13 +0000

    Wire Firebase config into site

---
+++ firebase-config.js
@@ -0,0 +1,10 @@
+window.FNL_FIREBASE_CONFIG = {
+  apiKey: "AIzaSyD3X8KecK7b7ZNZWDusNzO9do0IlN-jNQE",
+  authDomain: "football-nine-league.firebaseapp.com",
+  projectId: "football-nine-league",
+  storageBucket: "football-nine-league.firebasestorage.app",
+  messagingSenderId: "172532669165",
+  appId: "1:172532669165:web:6ff9ddc4b481e938954ae4"
+};
+
+window.FNL_FIREBASE_APP_ID = "fnl-season1";
