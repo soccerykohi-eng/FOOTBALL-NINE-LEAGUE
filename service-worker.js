f49cb8a4a95030e1885180586d15329d1e0f9c74
soccerykohi-eng <soccerykohi@gmail.com>
2026-07-26 13:54:17 +0900

    Limit transfer history display

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'fnl-app-v7';
+const CACHE_NAME = 'fnl-app-v8';
 const APP_SHELL = ['./', './index.html', './manifest.webmanifest', './fnl-logo.png'];
 
 self.addEventListener('install', event => {
