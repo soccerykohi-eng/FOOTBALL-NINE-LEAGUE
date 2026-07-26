a2ed0a56302ac296f7a299192da66d0c2dee06e4
soccerykohi-eng <soccerykohi@gmail.com>
2026-07-26 15:20:14 +0900

    Limit player records to top five

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'fnl-app-v13';
+const CACHE_NAME = 'fnl-app-v14';
 const APP_SHELL = ['./', './index.html', './manifest.webmanifest', './fnl-logo.png'];
 
 self.addEventListener('install', event => {
