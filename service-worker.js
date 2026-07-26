d9f530fade872c466ced1478c79809c4eee30aac
soccerykohi-eng <soccerykohi@gmail.com>
2026-07-26 14:10:36 +0900

    Show club stats and media in team details

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'fnl-app-v9';
+const CACHE_NAME = 'fnl-app-v10';
 const APP_SHELL = ['./', './index.html', './manifest.webmanifest', './fnl-logo.png'];
 
 self.addEventListener('install', event => {
