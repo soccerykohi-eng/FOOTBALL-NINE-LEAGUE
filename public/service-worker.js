a95dc92e0b60719b9ff33c7e081aa817eadbd82b
soccerykohi-eng <soccerykohi@gmail.com>
2026-07-28 21:50:25 +0900

    Remove team detail summary metrics

--- public/service-worker.js
+++ public/service-worker.js
@@ -14,7 +14,7 @@ try {
   console.warn('Firebase Messaging could not be initialized', error);
 }
 
-const CACHE_NAME = 'fnl-app-v39';
+const CACHE_NAME = 'fnl-app-v40';
 const APP_SHELL = ['./', './index.html', './manifest.webmanifest', './fnl-logo.png'];
 
 self.addEventListener('install', event => {
