947830e3d7d90b32aa004ece286a2568346f8bbd
soccerykohi-eng <soccerykohi@gmail.com>
2026-07-28 22:45:49 +0900

    Show current regulations when opening rules

--- public/service-worker.js
+++ public/service-worker.js
@@ -14,7 +14,7 @@ try {
   console.warn('Firebase Messaging could not be initialized', error);
 }
 
-const CACHE_NAME = 'fnl-app-v47';
+const CACHE_NAME = 'fnl-app-v48';
 const APP_SHELL = ['./', './index.html', './manifest.webmanifest', './fnl-logo.png'];
 
 self.addEventListener('install', event => {
