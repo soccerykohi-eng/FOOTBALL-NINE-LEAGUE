ea9f739a477e06883887b93bd9905f2a59578bbe
soccerykohi-eng <soccerykohi@gmail.com>
2026-07-28 21:45:49 +0900

    Simplify team player stats heading

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@ try {
   console.warn('Firebase Messaging could not be initialized', error);
 }
 
-const CACHE_NAME = 'fnl-app-v38';
+const CACHE_NAME = 'fnl-app-v39';
 const APP_SHELL = ['./', './index.html', './manifest.webmanifest', './fnl-logo.png'];
 
 self.addEventListener('install', event => {
