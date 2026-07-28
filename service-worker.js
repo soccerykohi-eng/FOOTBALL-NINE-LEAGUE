c3263c572c6a5e7e8816c01e47d81be30e427316
soccerykohi-eng <soccerykohi@gmail.com>
2026-07-28 23:08:26 +0900

    Refresh FNL visual system

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@ try {
   console.warn('Firebase Messaging could not be initialized', error);
 }
 
-const CACHE_NAME = 'fnl-app-v51';
+const CACHE_NAME = 'fnl-app-v52';
 const APP_SHELL = ['./', './index.html', './manifest.webmanifest', './fnl-logo.png'];
 
 self.addEventListener('install', event => {
