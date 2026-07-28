c4337de8cec63d79f6ca79bb3470a07617984864
soccerykohi-eng <soccerykohi@gmail.com>
2026-07-28 21:27:12 +0900

    Add team player stat tabs

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@ try {
   console.warn('Firebase Messaging could not be initialized', error);
 }
 
-const CACHE_NAME = 'fnl-app-v36';
+const CACHE_NAME = 'fnl-app-v37';
 const APP_SHELL = ['./', './index.html', './manifest.webmanifest', './fnl-logo.png'];
 
 self.addEventListener('install', event => {
