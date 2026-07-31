55c33a8c0f26fcb29932c7ba993f88d2299c254d
soccerykohi-eng <soccerykohi@gmail.com>
2026-07-31 19:42:31 +0900

    Use FNL logo as browser icon

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@ try {
   console.warn('Firebase Messaging could not be initialized', error);
 }
 
-const CACHE_NAME = 'fnl-app-v65';
+const CACHE_NAME = 'fnl-app-v66';
 const APP_SHELL = ['./', './index.html', './manifest.webmanifest', './fnl-logo.png'];
 
 self.addEventListener('install', event => {
