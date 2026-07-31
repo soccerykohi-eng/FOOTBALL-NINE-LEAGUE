6448f91f61221e603ecb6523848bbd45ca23a941
soccerykohi-eng <soccerykohi@gmail.com>
2026-07-31 19:10:40 +0900

    Remove event time input

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@ try {
   console.warn('Firebase Messaging could not be initialized', error);
 }
 
-const CACHE_NAME = 'fnl-app-v63';
+const CACHE_NAME = 'fnl-app-v64';
 const APP_SHELL = ['./', './index.html', './manifest.webmanifest', './fnl-logo.png'];
 
 self.addEventListener('install', event => {
