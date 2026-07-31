da852daef17e11f159461794b8e265d53468300f
soccerykohi-eng <soccerykohi@gmail.com>
2026-07-31 19:20:54 +0900

    Refine news reading experience

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@ try {
   console.warn('Firebase Messaging could not be initialized', error);
 }
 
-const CACHE_NAME = 'fnl-app-v64';
+const CACHE_NAME = 'fnl-app-v65';
 const APP_SHELL = ['./', './index.html', './manifest.webmanifest', './fnl-logo.png'];
 
 self.addEventListener('install', event => {
