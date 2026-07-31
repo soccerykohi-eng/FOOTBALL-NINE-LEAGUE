5d70cc7b9b10181c87c33e1504b708e9c7145feb
soccerykohi-eng <soccerykohi@gmail.com>
2026-07-31 18:50:28 +0900

    Simplify match center save guidance

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@ try {
   console.warn('Firebase Messaging could not be initialized', error);
 }
 
-const CACHE_NAME = 'fnl-app-v60';
+const CACHE_NAME = 'fnl-app-v61';
 const APP_SHELL = ['./', './index.html', './manifest.webmanifest', './fnl-logo.png'];
 
 self.addEventListener('install', event => {
