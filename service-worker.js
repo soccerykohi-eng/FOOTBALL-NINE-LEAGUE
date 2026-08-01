3c0b1fbe0a7ea7a534063ec093f07a366991fbf7
soccerykohi-eng <soccerykohi@gmail.com>
2026-08-01 22:25:03 +0900

    Use MOM badge instead of emoji

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@ try {
   console.warn('Firebase Messaging could not be initialized', error);
 }
 
-const CACHE_NAME = 'fnl-app-v73';
+const CACHE_NAME = 'fnl-app-v74';
 const APP_SHELL = ['./', './index.html', './manifest.webmanifest', './fnl-logo.png', './hirabayashi-cup.png'];
 
 self.addEventListener('install', event => {
