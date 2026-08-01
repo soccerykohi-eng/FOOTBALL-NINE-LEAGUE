812a64ae57530b1694a897ad62a7dcb7a4aa11da
soccerykohi-eng <soccerykohi@gmail.com>
2026-08-01 22:23:13 +0900

    Remove optional labels from match center

--- public/service-worker.js
+++ public/service-worker.js
@@ -14,7 +14,7 @@ try {
   console.warn('Firebase Messaging could not be initialized', error);
 }
 
-const CACHE_NAME = 'fnl-app-v72';
+const CACHE_NAME = 'fnl-app-v73';
 const APP_SHELL = ['./', './index.html', './manifest.webmanifest', './fnl-logo.png', './hirabayashi-cup.png'];
 
 self.addEventListener('install', event => {
