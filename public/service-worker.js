ad41673c1d03a501538be159f619480db5e87d32
soccerykohi-eng <soccerykohi@gmail.com>
2026-08-02 13:58:50 +0900

    Fit standings on one phone screen

--- public/service-worker.js
+++ public/service-worker.js
@@ -14,7 +14,7 @@ try {
   console.warn('Firebase Messaging could not be initialized', error);
 }
 
-const CACHE_NAME = 'fnl-app-v88';
+const CACHE_NAME = 'fnl-app-v89';
 const APP_SHELL = ['./', './index.html', './manifest.webmanifest', './fnl-logo.png', './hirabayashi-cup.png'];
 
 self.addEventListener('install', event => {
