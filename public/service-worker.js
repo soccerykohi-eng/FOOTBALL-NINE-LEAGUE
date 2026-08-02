3635a8473ab0683b35f9d8b93a547dc567c1bed9
soccerykohi-eng <soccerykohi@gmail.com>
2026-08-02 14:30:48 +0900

    Refine Hirabayashi Cup guide typography

--- public/service-worker.js
+++ public/service-worker.js
@@ -14,7 +14,7 @@ try {
   console.warn('Firebase Messaging could not be initialized', error);
 }
 
-const CACHE_NAME = 'fnl-app-v90';
+const CACHE_NAME = 'fnl-app-v91';
 const APP_SHELL = ['./', './index.html', './manifest.webmanifest', './fnl-logo.png', './hirabayashi-cup.png'];
 
 self.addEventListener('install', event => {
