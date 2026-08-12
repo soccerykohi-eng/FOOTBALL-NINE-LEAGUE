69b5d1a71accc7dafce6ecff9a297bc1bffe3d69
soccerykohi-eng <soccerykohi@gmail.com>
2026-08-12 23:30:59 +0900

    Simplify transfer center workflow

--- public/service-worker.js
+++ public/service-worker.js
@@ -14,7 +14,7 @@ try {
   console.warn('Firebase Messaging could not be initialized', error);
 }
 
-const CACHE_NAME = 'fnl-app-v110';
+const CACHE_NAME = 'fnl-app-v111';
 const APP_SHELL = ['./', './index.html', './manifest.webmanifest', './fnl-logo.png', './hirabayashi-cup.png'];
 
 self.addEventListener('install', event => {
